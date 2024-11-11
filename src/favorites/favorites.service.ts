e88212a01fd159d2109bf9eb3f21eeab216e9cc9
inessa <inessasmorgunova@gmail.com>
2024-11-11 18:53:10 -0500

    fix: remove redundant code

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -82,9 +82,4 @@ export class FavoritesService {
     }
     return false;
   }
-  private isValidUuid(id: string): boolean {
-    const uuidRegex =
-      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
-    return uuidRegex.test(id);
-  }
 }
