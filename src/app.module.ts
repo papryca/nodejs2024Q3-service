c0ff451a9097e04a35ddb8fb7648985a81a4999e
inessa <inessasmorgunova@gmail.com>
2024-11-16 12:26:18 -0500

    feat: add dockerignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { TracksModule } from './tracks/tracks.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     UsersModule,
     TracksModule,
     ArtistModule,
