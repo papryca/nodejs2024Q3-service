0cb9eded6a873f17b90328137a17b23cab8c5f1b
inessa <inessasmorgunova@gmail.com>
2024-11-25 15:12:02 -0500

    feat: add exeption filter

---
+++ src/logging/exception-filter.ts
@@ -0,0 +1,38 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { LoggingService } from './logging.service';
+
+@Catch()
+export class CustomExceptionFilter implements ExceptionFilter {
+  constructor(private readonly loggingService: LoggingService) {}
+
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const errorResponse = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message: exception.message || 'Internal Server Error',
+    };
+
+    const logMessage = `HTTP ${status} Error: ${exception.message}\nStack: ${
+      exception.stack || 'No stack available'
+    }`;
+    this.loggingService.error(logMessage);
+
+    response.status(status).json(errorResponse);
+  }
+}
