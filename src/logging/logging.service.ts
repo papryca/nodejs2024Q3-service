d96af0e3a4fd16da14e970b8d40d59b76469348c
inessa <inessasmorgunova@gmail.com>
2024-11-25 17:23:48 -0500

    feat: add levels

--- src/logging/logging.service.ts
+++ src/logging/logging.service.ts
@@ -1,9 +1,16 @@
-import { ConsoleLogger, Injectable } from "@nestjs/common";
+import {
+  ConsoleLogger,
+  Injectable,
+  LoggerService,
+  LogLevel,
+} from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
+import { getActiveLogLevels } from './logger-config';
 
 @Injectable()
-export class LoggingService extends ConsoleLogger {
+export class LoggingService extends ConsoleLogger implements LoggerService {
+  private readonly activeLevels: LogLevel[] = getActiveLogLevels();
   private readonly logFilePath = path.join(
     __dirname,
     '..',
@@ -18,20 +25,53 @@ export class LoggingService extends ConsoleLogger {
     'logging',
     'error.log',
   );
-
+  private shouldLog(level: LogLevel): boolean {
+    return this.activeLevels.includes(level);
+  }
   log(message: string) {
-    super.log(message);
-    fs.appendFileSync(
-      this.logFilePath,
-      `${new Date().toISOString()} - LOG: ${message}\n`,
-    );
+    if (this.shouldLog('log')) {
+      super.log(message);
+      fs.appendFileSync(
+        this.logFilePath,
+        `${new Date().toISOString()} - LOG: ${message}\n`,
+      );
+    }
+  }
+  warn(message: string) {
+    if (this.shouldLog('warn')) {
+      super.warn(message);
+      fs.appendFileSync(
+        this.logFilePath,
+        `${new Date().toISOString()} - WARN: ${message}\n`,
+      );
+    }
+  }
+  debug(message: string) {
+    if (this.shouldLog('debug')) {
+      super.debug(message);
+      fs.appendFileSync(
+        this.logFilePath,
+        `${new Date().toISOString()} - DEBUG: ${message}\n`,
+      );
+    }
+  }
+  verbose(message: string) {
+    if (this.shouldLog('verbose')) {
+      super.verbose(message);
+      fs.appendFileSync(
+        this.logFilePath,
+        `${new Date().toISOString()} - VERBOSE: ${message}\n`,
+      );
+    }
   }
 
   error(message: string) {
-    super.error(message);
-    fs.appendFileSync(
-      this.errorFilePath,
-      `${new Date().toISOString()} - LOG: ${message}\n`,
-    );
+    if (this.shouldLog('error')) {
+      super.error(message);
+      fs.appendFileSync(
+        this.errorFilePath,
+        `${new Date().toISOString()} - ERROR: ${message}\n`,
+      );
+    }
   }
 }
